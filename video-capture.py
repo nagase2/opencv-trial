20e5f43a516faab6494eff1e2b1b3ff45e278570
yoshi-PC\yoshi <nagase2@gmail.com>
2017-07-23 01:35:51 +0900

    動画を読み込むサンプルを追加した。

--- video-capture.py
+++ video-capture.py
@@ -1,12 +1,17 @@
 import numpy as np
 import cv2
-
-
+'''
+ビデオファイルを読み込み、グレイで表示する散布売る
+'''
 cap = cv2.VideoCapture('images/test.MOV')
+
+
 
 while(True):
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # TODO:動画に線を追加したいが、うまく動かない。
+    #cv2.line(cap, (0, 0), (150, 150), (255, 255, 255), 15)
 
     cv2.imshow('frame',gray)
     if cv2.waitKey(1) & 0xFF == ord('q'):
