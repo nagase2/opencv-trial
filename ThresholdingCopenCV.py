c2777c12d3079793d70ca3c5a97bad0bb82b70c1
yoshi-PC\yoshi <nagase2@gmail.com>
2017-07-23 12:20:10 +0900

    グレイスケールサンプル

--- ThresholdingCopenCV.py
+++ ThresholdingCopenCV.py
@@ -1,16 +1,14 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('images/singapore.jpg')
+#img = cv2.imread('images/singapore.jpg')
+img = cv2.imread('images/book.png')
 
-#show thresholded image
-#retval, threshold = cv2.threshold(img, 50,255,cv2.THRESH_BINARY)
 
 #show grayschaled image
-img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-retval, threshold = cv2.threshold(img, 10,255,cv2.THRESH_BINARY)
-
+grayscaled = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+th = cv2.adaptiveThreshold(grayscaled,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,115,1)
 cv2.imshow('original',img)
-cv2.imshow('threshold',threshold)
+cv2.imshow('threshold',th)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
