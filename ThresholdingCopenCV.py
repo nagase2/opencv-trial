fbcf4aa400ad6c70b4f3995abbb5f9c9d987796d
yoshi-PC\yoshi <nagase2@gmail.com>
2017-07-23 13:19:26 +0900

    adaptiveカラーチェンジ

--- ThresholdingCopenCV.py
+++ ThresholdingCopenCV.py
@@ -4,7 +4,7 @@ import numpy as np
 #img = cv2.imread('images/singapore.jpg')
 img = cv2.imread('images/book.png')
 
-
+[]
 #show grayschaled image。
 grayscaled = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 th = cv2.adaptiveThreshold(grayscaled,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,115,1)
