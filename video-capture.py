3cb2fcaea381372b87ad7b1d222684dea5b33f17
yoshi-PC\yoshi <nagase2@gmail.com>
2017-07-23 12:14:47 +0900

    グレイスケールサンプル

--- video-capture.py
+++ video-capture.py
@@ -10,7 +10,7 @@ cap = cv2.VideoCapture('images/test.MOV')
 while(True):
     ret, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # TODO:動画に線を追加したいが、うまく動かない。
+    # TODO: I wanted to drow a line onto the movie, but couldn't make it so far.
     #cv2.line(cap, (0, 0), (150, 150), (255, 255, 255), 15)
 
     cv2.imshow('frame',gray)
