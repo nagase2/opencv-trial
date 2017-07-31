d07828dad64588ee2f1effd7d0983381007b75e7
Naggi <nagase2@gmail.com>
2017-07-31 22:40:21 +0900

    Webスクレイピングプログラムを追加

---
+++ webscraping/FizzBuzz.py
@@ -0,0 +1,25 @@
+
+
+
+def fizzbuzz(num):
+  if num % 3 == 0:
+      return 'fizz'
+  elif num%5 ==0:
+      return 'buzz'
+  else:
+      return num
+
+
+
+
+for num in range(1, 101):
+    print(fizzbuzz(num))
+
+
+
+
+
+
+
+
+
