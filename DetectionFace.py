710902772e813e0b6f2eeeca5521b2f6132c54e0
yoshi-PC\yoshi <nagase2@gmail.com>
2017-08-11 16:20:57 +0900

    いくつかのOPEN CVサンプルを追加した。

--- DetectionFace.py
+++ DetectionFace.py
@@ -3,8 +3,8 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-face_cascade = cv2.CascadeClassifier('xml/haarcascade_frontalface_default')
-eye_cascade =  cv2.CascadeClassifier('xml/haarcascade_eye.xml')
+face_cascade = cv2.CascadeClassifier('C:/Users/yoshi/PycharmProjects/opencv-trial/xml/haarcascade_frontalface_default.xml')
+eye_cascade =  cv2.CascadeClassifier('C:/Users/yoshi/PycharmProjects/opencv-trial/xml/haarcascade_eye.xml')
 
 #cap=cv2.VideoCaptsure(0)
 cap = cv2.VideoCapture('images/test.MOV')
@@ -12,7 +12,7 @@ cap = cv2.VideoCapture('images/test.MOV')
 while 1:
     ret,img = cap.read()
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray,1.3,5)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
     for (x,y,w,h) in faces:
         cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
         roi_gray = gray[y:y+h, x:x+w]
